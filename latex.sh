dcdfbbe0d9671e51dcf997c6a499a7808e057d7f
Magnus <mag.aars@gmail.com>
2015-04-12 12:06:28 +0200

    latex - added

---
+++ latex.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Getting latex to work
+
+# Install pdflatex
+sudo apt-get install texlive-latex-base
+
+# Get packages.
+# If you need to install something more, just type the name of the package in the package manager, and install it. 
+sudo apt-get install texlive
+sudo apt-get install texlive-latex-extra
+sudo apt-get install latex-xcolor
+sudo apt-get install pgf # To get tikz
+sudo apt-get install texlive-lang-norwegian
+
